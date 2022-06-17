48fb82c0d205bd79694bcf769c456cfe3eb79eb5
Vu Ngoc Thinh (K14 HL) <thinhvnhe141345@fpt.edu.vn>
2022-06-17 12:00:41 +0000

    customer.js edited online with Bitbucket

--- src/api/customer.js
+++ src/api/customer.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const instance = axios.create({
-  baseURL: 'https://flix-lj7prqscta-uc.a.run.app/api/v1',
+  baseURL: 'https://flix-lj7prqscta-as.a.run.app/api/v1',
 });
 instance.interceptors.request.use(
   async config => {
