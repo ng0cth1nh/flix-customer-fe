c43c240778b51f29c419e7781c1670fca9cd68c2
Vu Ngoc Thinh (K14 HL) <thinhvnhe141345@fpt.edu.vn>
2022-06-17 12:02:16 +0000

    Api.js edited online with Bitbucket

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,14 +1,14 @@
 const ApiConstants = {
-  LOGIN_API: 'https://flix-lj7prqscta-uc.a.run.app/api/v1/login',
-  SEND_OTP_API: 'https://flix-lj7prqscta-uc.a.run.app/api/v1/register/sendOTP',
+  LOGIN_API: 'https://flix-lj7prqscta-as.a.run.app/api/v1/login',
+  SEND_OTP_API: 'https://flix-lj7prqscta-as.a.run.app/api/v1/register/sendOTP',
   CONFIRM_OTP_API:
-    'https://flix-lj7prqscta-uc.a.run.app/api/v1/register/confirm',
+    'https://flix-lj7prqscta-as.a.run.app/api/v1/register/confirm',
   REFRESH_TOKEN_API:
-    'https://flix-lj7prqscta-uc.a.run.app/api/v1/token/refresh',
-  GET_ALL_CITY_API: 'https://flix-lj7prqscta-uc.a.run.app/api/v1/address/city',
+    'https://flix-lj7prqscta-as.a.run.app/api/v1/token/refresh',
+  GET_ALL_CITY_API: 'https://flix-lj7prqscta-as.a.run.app/api/v1/address/city',
   GET_DISTRICT_BY_CITY_API:
-    'https://flix-lj7prqscta-uc.a.run.app/api/v1/address/district',
+    'https://flix-lj7prqscta-as.a.run.app/api/v1/address/district',
   GET_COMMUNE_BY_DISTRICT_API:
-    'https://flix-lj7prqscta-uc.a.run.app/api/v1/address/commune',
+    'https://flix-lj7prqscta-as.a.run.app/api/v1/address/commune',
 };
 export default ApiConstants;
